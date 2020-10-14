bbd0f889d4a27c7d965e4d49df961196460ce578
Lucas Mauro <lucas_de_mauro@hotmail.com>
2020-10-14 16:23:50 -0300

    Mapbox

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -218,9 +218,13 @@ const CreatePoint = () => {
             zoom={15} 
             onClick={handleMapClick}
           >
-            <TileLayer
+            {/* <TileLayer
               attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            /> */}
+
+            <TileLayer 
+              url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}
             />
 
             <Marker position={selectedPosition} />
